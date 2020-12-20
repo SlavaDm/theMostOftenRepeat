834df0f5d1ee2a6d350e325ae0553f649253d0cf
SlavaDm <65130162+SlavaDm@users.noreply.github.com>
2020-12-20 19:28:13 +0700

    Create theMostOftenRepeat.js

---
+++ theMostOftenRepeat.js
@@ -0,0 +1,25 @@
+const b = []
+const arr = [1, 5, 3, 6, 2, 7, 8, 1, 4, 6, 1]
+
+arr.forEach(el => {
+  let c = false
+
+  for (let i of b) {
+    if (i.value == el) {
+      i.repeat++
+      c = true
+    }
+  }
+
+  if (c === false) {
+    b.push({ value: el, repeat: 1 })
+  }
+})
+
+const findTheMostOftenRepeat = (value = 1) => {
+  return b.slice(0, value)
+}
+
+b.sort((a, b) => (a.repeat > b.repeat ? -1 : 1))
+
+console.log(findTheMostOftenRepeat(3))
